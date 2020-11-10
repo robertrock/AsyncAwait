90e2175b7ae74acc464747da6f124bf519efe308
Robert Thomas <RobertThomas31416@gmail.com>
2020-11-09 22:17:13 -0600

    Add project files.

---
+++ AsyncAwait/WebsiteDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    public class WebsiteDataModel
+    {
+        public string WebsiteUrl { get; set; } = "";
+        public string WebsiteData { get; set; }
+    }
+}
